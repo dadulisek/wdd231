8fc8e97eeec3cfd7db5717c42cff18b7b7c357c2
dadulisek <navratildada@gmail.com>
2025-01-11 13:30:22 -0700

    home page started creating css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -13,3 +13,14 @@ const formattedDate = lastModifiedDate.toLocaleDateString('en-US');
 const formattedTime = lastModifiedDate.toLocaleTimeString('en-US', { hour12: false });
 
 lastModifiedElement.textContent = `Last Modification: ${formattedDate} ${formattedTime}`;
+
+
+// header navigation code
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', ()=> {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
